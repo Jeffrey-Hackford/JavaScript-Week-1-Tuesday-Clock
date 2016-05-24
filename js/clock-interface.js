098e2e1d06ee7bb9bf63368d141ac2b89a60af43
Jeffrey Hackford and Ryan Harvey <jeffrey@>
2016-05-24 14:03:26 -0700

    Will change css styles when current time matches inputted user time

--- js/clock-interface.js
+++ js/clock-interface.js
@@ -1,6 +1,8 @@
 var Clock = require('./../js/clock.js').Clock;
 var moment = require('moment');
-
+var hour;
+var minute;
+var ampm;
 
 $(document).ready(function(){
   var newClock = new Clock();
@@ -13,20 +15,29 @@ $(document).ready(function(){
     $('#hour').append("<option value='" + i + "'>" + i + "</option>");
   }
 
-  for(var i = 1; i<= 60; i++){
+  for(var i = 1; i <= 60; i++){
     $('#minute').append("<option value='" + i + "'>" + i + "</option>");
   }
 
   $('#alarm').submit(function(event){
     event.preventDefault();
-    var hour = parseInt($('#hour').val());
-    var minute = parseInt($('#minute').val());
+    var hour = $('#hour').val();
+    var minute = $('#minute').val();
     var ampm = $("#ampm").val();
-
     if (minute < 10){
-      $('#userDisplay').html("<h3>Your alarm is set for " + hour + ":0" + minute + " " + ampm + "</h3>");
-    } else {
-      $('#userDisplay').html("<h3>Your alarm is set for " + hour + ":" + minute + " " + ampm + "</h3>");
+      minute = "0" + minute;
     }
+    $('#userDisplay').html("<h3>Your alarm is set for " + hour + ":" + minute + " " + ampm + "</h3>");
+
+  function alarm(){
+    var userTime = hour + ":" + minute + ":00 " + ampm;
+    var alarmString = moment().format('LTS').toString();
+    console.log(userTime);
+    console.log(alarmString);
+    if(alarmString === userTime){
+      $('body').addClass('flash');
+    }
+  }
+    setInterval(alarm, 1000);
   });
 });
